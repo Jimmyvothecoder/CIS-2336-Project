910679b81befa1e5da14684ef868e3a82345ddbf
Jimmy Vo <jtvo8@cougarnet.uh.edu>
2025-03-18 10:36:55 -0500

    inital commit

---
+++ front end/app1.js
@@ -0,0 +1,28 @@
+// create variable that calls data-tab- target
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+// create a function for an click event handler
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+            
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
+
+// create a function for a click event
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    })
+})
